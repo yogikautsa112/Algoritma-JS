96bde3ad2c6c6357885c13d1c7c9bfdd88fd4c66
yogikautsa112 <yogikautsaralnandeta@smkwikrama.sch.id>
2024-01-17 11:04:47 +0700

    add do-whie

---
+++ looping/loop.js
@@ -0,0 +1,60 @@
+// For
+// for (let x = 1; x <= 50; x++ ) {
+//     if(x % 2 == 0){
+//         console.log( `${x} adalah bilangan genap`)
+//     } else {
+//         console.log(`${x} adalah bilangan ganjil`)
+//     }
+// }
+
+// While
+// let x = 1
+// while( x <= 50) {
+//     if ( x % 2 == 0) {
+//         console.log(`${x} adalah bilangan genap`)
+//     } else {
+//         console.log(`${x} adalah bilangan ganjil`)
+//     }
+//     x++;
+// }
+
+// Do-While
+// let i = 1
+// do {
+//     if ( i % 2 == 0) {
+//         console.log(`${i} adalah bilangan genap`)
+//     } else {
+//         console.log(`${i} adalah bilangan ganjil`)
+//     }
+//     i++;
+// } while( i <= 50)
+
+// Faktorial 
+
+// const number = Number(prompt('Enter a number'))
+// let factor = 1
+
+// for (let j = 1; j <= number; j++) {
+//     factor *= j
+// }console.log(`Menggunakan for : ${number} faktorial nya adalah ${factor}`)
+
+// const total = Number(prompt('Enter a number'))
+// let facto = 1
+// let x = 1
+
+// while(x <= total) {
+//     facto *= x
+//     x++;
+// } 
+// console.log(`Menggunakan while : ${total} faktorial nya adalah ${facto}`)
+
+// const num = Number(prompt('Enter a number'))
+// let fact = 1
+// let i = 1
+
+// do {
+//     fact *= i
+//     i++
+// } while (i <= num)
+// console.log(`Menggunakan do-while : ${num} faktorial nya adalah ${fact}`)
+
