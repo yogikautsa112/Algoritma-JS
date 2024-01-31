26365bd8d533465b566ef4c882abfde1b2fa2859
yogikautsa112 <yogikautsaralnandeta@smkwikrama.sch.id>
2024-01-31 12:34:05 +0700

    Update

--- LKPD 5/index.js
+++ LKPD 5/index.js
@@ -1,55 +1,54 @@
 // No 1 
-// for(let i = 1;i <= 50; i++) {
-//     console.log(i)
-// }
+for(let i = 1;i <= 50; i++) {
+    console.log(i)
+}
 
 // No 2
-// let j = 1
-// while( j <= 50) {
-//     if ( j % 2 !== 0) {
-//         console.log(`${j} bilangan ganjil`)
-//     }
-// }
+let j = 1
+while( j <= 50) {
+    if ( j % 2 !== 0) {
+        console.log(`${j} bilangan ganjil`)
+    }
+}
 
 // NO 3
-// let y = 1
-// while ( y <= 50) {
-//     if ( y % 2 == 0) {
-//         console.log(`${y} bilangan genap`)
-//     }
-// }
+let y = 1
+while ( y <= 50) {
+    if ( y % 2 == 0) {
+        console.log(`${y} bilangan genap`)
+    }
+}
 
 // NO 4
-// let x = 1
-
-// while( x <= 50) {
-//     if ( x % 2 == 0) {
-//         console.log(`${2} Bilangan Genap`)
-//     } else {
-//         console.log(`${x} Bilangan Ganjil`)
-//     }
-// }
+let x = 1
+while( x <= 50) {
+    if ( x % 2 == 0) {
+        console.log(`${2} Bilangan Genap`)
+    } else {
+        console.log(`${x} Bilangan Ganjil`)
+    }
+}
 
 // No 5
-// let total = 0 
-// let min = Infinity
-// let max = -Infinity
+let total = 0 
+let min = Infinity
+let max = -Infinity
 
-// for( let i = 0;i <= 20; i++) {
-//     let input = parseInt(prompt('Input Bilangan', i))
-//     total += input
+for( let i = 0;i <= 20; i++) {
+    let input = parseInt(prompt('Input Bilangan', i))
+    total += input
 
-//     if ( input < min) {
-//         min = input
-//     }
+    if ( input < min) {
+        min = input
+    }
 
-//     if ( input > max) {
-//         max = input
-//     }
-// }
+    if ( input > max) {
+        max = input
+    }
+}
 
-// let rataRata = total / 20
+let rataRata = total / 20
 
-// console.log(`Bilangan Terbesar: ${max}`)
-// console.log(`Bilangan Terkecil: ${min}`)
-// console.log(`Rata-Rata: ${rataRata}`)
+console.log(`Bilangan Terbesar: ${max}`)
+console.log(`Bilangan Terkecil: ${min}`)
+console.log(`Rata-Rata: ${rataRata}`)
